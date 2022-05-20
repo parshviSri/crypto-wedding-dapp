0f079037f88f2370bb8428fa10ecd5a5cc4e95a0
Anil N <anil.narasipuram@gmail.com>
2022-05-20 22:06:28 +0400

    basic version of ring contract

--- contracts/Ring.sol
+++ contracts/Ring.sol
@@ -1,11 +1,33 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721.sol"; ??
+contract Ring is ERC721("CryptoWedding", "RING"), Ownable {
+    using Counters for Counters.Counter;
 
-contract Ring is ERC721Enumerable, Ownable {
-    // use ERC721Enumerable or ERC721 ??~
+    Counters.Counter internal _tokenIds;
+
+    string public baseURI;
+
+    constructor() {}
+
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
+
+    function setBaseURI(string memory _baseURIParam) public onlyOwner {
+        baseURI = _baseURIParam;
+    }
+
+    function mint(address to) public onlyOwner returns (uint256) {
+        uint256 ringId = _tokenIds.current();
+        _safeMint(to, ringId);
+
+        _tokenIds.increment();
+
+        return ringId;
+    }
 }
