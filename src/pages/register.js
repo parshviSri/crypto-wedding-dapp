f043fa2ef087e414fde189b395d45ddc8eb36a34
Parshvi <parshvisri@gmail.com>
2022-05-26 21:17:32 +0530

    Resolving Merging Issue

--- src/pages/register.js
+++ src/pages/register.js
@@ -1,7 +1,3 @@
-import { useState } from "react";
-import { ethers } from "ethers";
-import Wedding from "../../artifacts/contracts/WeddingManager.sol/WeddingManager.json";
-import { useRouter } from "next/router";
 
  import {useState} from 'react';
  import { ethers } from 'ethers';
@@ -52,61 +48,34 @@ import { useRouter } from "next/router";
         <div className='text-center'>
         <h2 className="font-medium leading-tight text-2xl mt-0 mb-2">Welcome</h2>
         <p> Get Started with your crypto wedding</p>
-      </div>
-      <div className="flex flex-row m-24 justify-center">
-        <div className="bg-white shadow-md rounded p-6 mb-4 ml-10">
-          <h1 className="text-center">Spouse 1</h1>
-          <div className="m-6">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="spouse name"
-              onChange={(e) =>
-                updateFormInput({ ...formInput, parterName1: e.target.value })
-              }
-            />
-          </div>
-          <div className="m-6">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="wallet address"
-              onChange={(e) =>
-                updateFormInput({ ...formInput, partner1: e.target.value })
-              }
-            />
-          </div>
         </div>
-        <div className="bg-white shadow-md rounded p-6 mb-4 ml-10">
-          <h1 className="text-center">Spouse 2</h1>
-          <div className="m-6">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="spouse name"
-              onChange={(e) =>
-                updateFormInput({ ...formInput, partnerName2: e.target.value })
-              }
-            />
-          </div>
-          <div className="m-6">
-            <input
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="wallet address"
-              onChange={(e) =>
-                updateFormInput({ ...formInput, partner2: e.target.value })
-              }
-            />
-          </div>
+        <div className='flex flex-row m-24 justify-center'>
+    <div className='bg-white shadow-md rounded p-6 mb-4 ml-10'>
+        <h1 className='text-center'>Spouse 1</h1>
+        <div className='m-6'>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder ="spouse name"onChange={e => updateFormInput({...formInput,parterName1:e.target.value})}/>
         </div>
-      </div>
-      <div className="m-6 flex items-center justify-center">
-        <button
-          className="bg-black hover:bg-gray-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          onClick={createWedding}
-        >
-          {" "}
-          Create Your Wedding
-        </button>
-      </div>
+        <div className='m-6' >
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder ="wallet address"onChange={e => updateFormInput({...formInput,partner1:e.target.value})}/>
+        </div>
+
     </div>
-  );
-};
+    <div className='bg-white shadow-md rounded p-6 mb-4 ml-10'>
+        <h1 className='text-center'>Spouse 2</h1>
+        <div className='m-6'>
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder ="spouse name"onChange={e => updateFormInput({...formInput,partnerName2:e.target.value})}/>
+        </div>
+        <div className='m-6' >
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' placeholder ="wallet address"onChange={e => updateFormInput({...formInput,partner2:e.target.value})}/>
+        </div>
+
+    </div>
+    </div>
+    <div className='m-6 flex items-center justify-center'>
+        <button className='bg-black hover:bg-gray-200 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' onClick={createWedding}> Create Your Wedding</button>
+
+        </div>
+    </div>
+    )
+}
 export default Register;
